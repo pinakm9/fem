db6e2b63c6ee544c42eab9964b4e21db48f8362b
pinakm9 <pinakm9@gmail.com>
2018-11-07 20:18:20 +0530

    experiment 1

--- plot.py
+++ plot.py
@@ -2,44 +2,43 @@ import matplotlib.pyplot as plt
 import numpy as np
 import subprocess, os, re, time
 import matplotlib.tri as tri
+import matplotlib as mpl
+import matplotlib.cm as cm
 
 class Plotter(object):
 	 
-	def __init__(self, mesh, left, right, xlabel = 'x', ylabel = 'y', id_ = ''):
+	def __init__(self, mesh, id_ = ''):
 		self.mesh = mesh
 		self.x = self.mesh.coordinates()[:,0] 
-		self.left = left
-		self.right = right
-		self.xlabel = xlabel
-		self.ylabel = ylabel
 		self.id = str(id_)
 		self.cmd = ['ffmpeg', '-y', '-framerate', '15', '-i', 'file_type', '-c:v', 'libx264', '-r', '30', '-pix_fmt', 'yuv420p', 'out_file']
 	 
-	def plot(self, func, dir_, itr, time, show = False):
+	def plot(self, func, dir_, itr, time, xlabel = 'x', ylabel = 'y', show = False):
 		if dir_[-1] != '/':
 			dir_ = dir_ + '/'
 		self.dir = dir_
 		self.func = np.vectorize(func) # func must be vectorized
 		self.y = self.func(self.x)
 		plt.clf()
-		plt.xlabel(self.xlabel)
-		plt.ylabel(self.ylabel)
+		plt.xlabel(xlabel)
+		plt.ylabel(ylabel)
+		self.quantity = ylabel
 		plt.plot(self.x, self.y, label = 'time = {:.3f}'.format(time))
 		plt.legend()
 		if show is True:
 			plt.show()
-		plt.savefig(self.dir + self.ylabel + '_' + self.id + '_' + '{:04.0f}'.format(itr))
+		plt.savefig(self.dir + self.quantity + '_' + self.id + '_' + '{:04.0f}'.format(itr))
 		print('Plot #{} generated'.format(itr), end = '\r')
 		
 
 	def purge(self):
 	    for file in os.listdir(self.dir):
-	        if re.search('^'+self.ylabel + '_' + self.id + '_\d*.png$', file):
+	        if re.search('^'+self.quantity + '_' + self.id + '_\d*.png$', file):
 	            os.remove(os.path.join(self.dir, file))
 
 	def create_video(self, delete = True):
-		file_type = self.ylabel + '_' + self.id + '_' + '%04d.png'
-		out_file = self.ylabel + '_' + self.id + '.mp4'
+		file_type = self.quantity + '_' + self.id + '_' + '%04d.png'
+		out_file = self.quantity + '_' + self.id + '.mp4'
 		self.cmd[5] = file_type
 		self.cmd[-1] = out_file
 		self.cwd = os.getcwd() + '/' + self.dir
@@ -48,33 +47,48 @@ class Plotter(object):
 		p.kill()
 		if delete is True:
 			self.purge()
-	
 
 class Plotter2(Plotter):
 
-	def __init__(self, mesh, left, right, xlabel = 'x', ylabel = 'y', id_ = ''):
-		super().__init__(mesh, left, right, xlabel, ylabel, id_)
+	def __init__(self, mesh, id_ = ''):
+		super().__init__(mesh, id_)
 		self.y = self.mesh.coordinates()[:,1]
-		self.triangulation = tri.Triangulation(self.x, self.y)
 
-	def plot(self, func, dir_, itr, time, show = False):
+	def plot(self, func, dir_, itr, time, xlabel = 'x', ylabel = 'y', quantity = '', show = False):
 		if dir_[-1] != '/':
 			dir_ = dir_ + '/'
 		self.dir = dir_
 		self.func = np.vectorize(func) # func must be vectorized
 		plt.clf()
-		plt.xlabel(self.xlabel)
-		plt.ylabel(self.ylabel) 
+		plt.cla()
+		plt.xlabel(xlabel)
+		plt.ylabel(ylabel)
+		self.quantity = quantity
 		z = self.func(self.x, self.y)
-		p = plt.tricontourf(self.triangulation, z, 20)
-		cbar = plt.colorbar(p)
+		# Create colorbar!
+		mu = np.mean(z)
+		std = np.std(z)
+		a, b = mu - 3*std, mu + 3*std
+		cmap = plt.get_cmap('PiYG')
+		m = plt.cm.ScalarMappable(cmap = cmap)
+		#plt.label('time = {:.3f}'.format(time))
+		m.set_array(z)
+		m.set_clim(a, b)
+		plt.colorbar(m, boundaries = np.arange(0,3))
+		p = plt.tricontourf(self.x, self.y, self.mesh.cells(), z, cmap = cmap)
+		plt.legend([quantity, 'time = {:.3f}'.format(time)])
+		#cbar = plt.colorbar()
 		if show is True:
 			plt.show()
-		plt.savefig(self.dir + self.ylabel + '_' + self.id + '_' + '{:04.0f}'.format(itr))
+		plt.savefig(self.dir + self.quantity + '_' + self.id + '_' + '{:04.0f}'.format(itr))
 		print('Plot #{} generated'.format(itr), end = '\r')
+		plt.close()
 
-	def stream(self, func):
+	def stream(self, func, dir_, itr, time, xlabel = 'x', ylabel = 'y', quantity = '', show = False):
 		if dir_[-1] != '/':
 			dir_ = dir_ + '/'
 		self.dir = dir_
-		self.func = np.vectorize(func)
+		self.func = np.vectorize(func) # func must be vectorized
+		plt.clf()
+		plt.xlabel(xlabel)
+		plt.ylabel(ylabel)
--- solve.py
+++ solve.py
@@ -10,19 +10,21 @@ kappa = Constant(0.285)
 M_s1 = Constant(3.6)
 A_1hat = Constant(0.4593)
 a_T = Constant(0.0314)
-Q = Constant(5)
+Q = Constant(0)
 beta = Constant(0.1)
 # Constants for the program
 T = 1  # final time
-num_steps = 100     # number of time steps
+num_steps = 50    # number of time steps
 dt = T / num_steps
 k = Constant(dt)
 f = 0.01
 
 # Define mesh
-mesh = Mesh("mesh/circle.xml")
+mesh = Mesh("mesh/propeller_2d_coarse.xml.gz")
+#plot(mesh)
+#plt.show()
 # Define function space
-P1 = FiniteElement('P', triangle, 2)
+P1 = FiniteElement('P', triangle, 3)
 element = MixedElement([P1, P1, P1])
 FS = FunctionSpace(mesh, element)
 
@@ -34,7 +36,7 @@ U = Function(FS)
 u, v, T = split(U)
 
 # Initial conditions
-U_n = interpolate(Expression(("pow(x[0],2)+x[1]", "cos(x[0]*x[1])", "sin(x[0]+x[1])"), degree = 1), FS)
+U_n = interpolate(Expression(("pow(x[0],2)+x[1]", "cos(x[0])+cos(x[1])", "sin(x[0]+x[1])"), degree = 1), FS)
 u_n, v_n, T_n = split(U_n)
 
 # Define variational problem
@@ -45,17 +47,15 @@ F = ((u - u_n) / k)*f_1*dx - f*v*f_1*dx + kappa*grad(T)[0]*f_1*dx - epsilon_1*u*
 
 # Solve the system for each time step
 t = 0
-pltr = Plotter2(mesh, -1, 1, ylabel = 'stf')
-temp = lambda x,y: u_n([x,y])
+pltr = Plotter2(mesh)
+temp = lambda x,y: v_n([x,y])
 for n in range(num_steps):
-    pltr.plot(temp,'qtcm1/temperature/', n, t)
     t += dt
+    if n%1 == 0:
+        pltr.plot(temp,'qtcm1/temperature/', n, t, quantity ='u-velocity')
     # Solve variational problem for time step
     J = derivative(F, U)
     solve(F == 0, U, J = J)
-    # Save solution to file (VTK)
-
-    # Update previous solution
     U_n.assign(U)
 
-pltr.create_video()
+#pltr.create_video()
